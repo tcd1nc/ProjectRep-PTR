b8378f75c6de0abe89798f4e0e7fda5e34c840b1
Terry <tcdj1963@gmail.com>
2019-11-01 13:38:50 +1300

    Add files via upload

---
+++ Views/PTMainView.xaml.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Windows;
+
+namespace PTR
+{
+    /// <summary>
+    /// Interaction logic for PTMainView.xaml
+    /// </summary>
+    public partial class PTMainView : Window
+    {
+
+        public PTMainView()
+        {                                           
+            InitializeComponent();
+            this.DataContext = new ViewModels.PTMainViewModel();
+            
+            this.Loaded += new RoutedEventHandler(PTMainView_Loaded);
+        }
+        void PTMainView_Loaded(object sender, RoutedEventArgs e)
+        {
+           (this.DataContext as ViewModels.PTMainViewModel).ShowReminderScreen();
+        }
+    }
+
+}
+
---
+++ Views/ProjectView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+
+namespace PTR.Views
+{
+    /// <summary>
+    /// Interaction logic for ProjectView.xaml
+    /// </summary>
+    public partial class ProjectView : Window
+    {
+       
+        public ProjectView()
+        {
+            InitializeComponent();
+            //new project
+            
+            this.DataContext = new ViewModels.ProjectViewModel();
+           
+        }
+
+        public ProjectView(int projectID)
+        {
+            InitializeComponent();
+           
+            //existing project
+            this.DataContext = new ViewModels.ProjectViewModel(projectID);
+           
+        }
+               
+    }
+}

5f95df950a1c808087cb8d33d3f626a78e7d192f
Terry <tcdj1963@gmail.com>
2020-05-25 20:19:28 +1200

    Add files via upload

--- Views/PlaybookView.xaml.cs
+++ Views/PlaybookView.xaml.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using PTR.Models;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Media;
-using static PTR.Constants;
 
 namespace PTR.Views
 {
@@ -14,95 +14,152 @@ namespace PTR.Views
     /// </summary>
     public partial class PlaybookView : Window
     {
-  
+        DataTable dt;
+        Dictionary<string, FilterPopupModel> dictFilterPopup;
+        //Dictionary<string, FilterPopupModel> dictFilterNewBusinessPopup;
         public PlaybookView()
         {
-            InitializeComponent();
-            this.DataContext = new ViewModels.PlaybookViewModel();
-            
-            ((ViewModels.PlaybookViewModel)DataContext).PropertyChanged += PlaybookView_PropertyChanged;
+            InitializeComponent();            
+            try
+            {
+                this.DataContext = new ViewModels.PlaybookViewModel();
+                ((ViewModels.PlaybookViewModel)DataContext).PropertyChanged += PlaybookView_PropertyChanged;
+                dt = (DataContext as ViewModels.PlaybookViewModel).SalesFunnel;
+                dictFilterPopup = (DataContext as ViewModels.PlaybookViewModel).DictFilterPopup;
 
-            UpdateData((DataContext as ViewModels.PlaybookViewModel).SalesFunnel);
-            UpdateData((DataContext as ViewModels.PlaybookViewModel).NewBusiness);
+                //dictFilterNewBusinessPopup = (DataContext as ViewModels.PlaybookViewModel).NewBusinessDictFilterPopup;
+            }
+            catch { }     
         }
-
+                            
         private void PlaybookView_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            if(e.PropertyName=="SalesFunnel")
-                UpdateData((DataContext as ViewModels.PlaybookViewModel).SalesFunnel);
-            else
-            if (e.PropertyName == "NewBusiness")
-                UpdateData((DataContext as ViewModels.PlaybookViewModel).NewBusiness);
-        }
-               
-        public void UpdateData(DataTable _dt)
-        {           
-            string _tablename = string.Empty;
-            
-            GridView gridView = new GridView();
-            GridViewColumn gridviewcol;
-            for (int i = 0; i < _dt.Columns.Count; i++)
-                {
-                                
-                gridviewcol = new GridViewColumn();
-                gridviewcol.Header = _dt.Columns[i].Caption;
-                gridviewcol.Width = Double.NaN;
-
-                Binding bind = new Binding(_dt.Columns[i].ColumnName);
-                bind.Mode = BindingMode.OneTime;
-
-                DataTemplate cell = new DataTemplate(); // create a datatemplate
-                FrameworkElementFactory factory = new FrameworkElementFactory(typeof(TextBlock));
-                factory.SetBinding(TextBlock.TextProperty, bind);//the second parameter should be bind   
-                factory.SetValue(TextBlock.PaddingProperty, new Thickness(2,0,2,0));
-                          
-                FrameworkElementFactory bdrfactory = new FrameworkElementFactory(typeof(Border));
-                bdrfactory.SetValue(Border.BorderBrushProperty, new SolidColorBrush(Colors.Gray));
-                bdrfactory.SetValue(Border.BorderThicknessProperty, new Thickness(0, 0, 0.5, 0.5));
-                bdrfactory.SetValue(Border.MarginProperty, new Thickness(-6, -2, -6, -2));
-                bdrfactory.AppendChild(factory);
-                if (_dt.Columns[i].DataType.Equals(typeof(DateTime)))
-                    bind.StringFormat = "d MMM yyyy";// "d";
-                else
-                {
-                    if (_dt.Columns[i].DataType.Equals(typeof(int)) || _dt.Columns[i].DataType.Equals(typeof(decimal)))
-                        if(_dt.Columns[i].ExtendedProperties.ContainsKey("Format"))
-                        bind.StringFormat = "{0:" + _dt.Columns[i].ExtendedProperties["Format"].ToString() + "}";
-                }
-
-                if (_dt.Columns[i].ExtendedProperties["Alignment"].ToString() == "Left")
-                    factory.SetValue(TextBlock.TextAlignmentProperty, TextAlignment.Left);
-                else                    
-                    if (_dt.Columns[i].ExtendedProperties["Alignment"].ToString() == "Right")
-                    factory.SetValue(TextBlock.TextAlignmentProperty, TextAlignment.Right);
-                else
-                    factory.SetValue(TextBlock.TextAlignmentProperty, TextAlignment.Center);
-
-                cell.VisualTree = bdrfactory;
-                gridviewcol.CellTemplate = cell;
-
-                gridView.Columns.Add(gridviewcol);
-            }
-
-            ListView _lv = null;
-
-            switch (_dt.TableName)
+            try
             {
-                case SalesFunnel:
-                    _lv = lvSalesFunnel;
-                    break;
-                case NewBusiness:
-                    _lv = lvNewBusiness;
-                    break;
-            }
-            _lv.BeginInit();
-            _lv.View = gridView;
-            _lv.ItemsSource = _dt.DefaultView;
-            _lv.EndInit();                               
+                dt = (DataContext as ViewModels.PlaybookViewModel).SalesFunnel;
+                dictFilterPopup = (DataContext as ViewModels.PlaybookViewModel).DictFilterPopup;
+                //dictFilterNewBusinessPopup = (DataContext as ViewModels.PlaybookViewModel).NewBusinessDictFilterPopup;
 
-            
+            }
+            catch { }
         }
        
+        private void ReportGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            try
+            {
+                var f = new FrameworkElementFactory(typeof(TextBlock));
+                Binding b = new Binding(e.Column.Header.ToString())
+                {
+                    Mode = BindingMode.OneTime
+                };
+                string colname = e.PropertyName;
+                switch (colname)
+                {
+                    case "SalesFunnelStage":
+                        f.SetValue(TextBlock.TextAlignmentProperty, TextAlignment.Center);
+                        f.SetValue(TextBlock.TextProperty, b);
+    
+                        if (!dictFilterPopup.ContainsKey(colname))
+                            dictFilterPopup.Add(colname, new FilterPopupModel() { ColumnName = colname, Caption = dt.Columns[colname].Caption, IsApplied = false });
+
+                        FilterPopupModel s = new FilterPopupModel();
+                        bool success = dictFilterPopup.TryGetValue(colname, out s);
+
+                        foreach (DataRow dr in dt.Rows)
+                            if (s.FilterData.Count(x => x.Description == dr[colname].ToString()) == 0 && !string.IsNullOrEmpty(dr[colname].ToString()))
+                                s.FilterData.Add(new FilterPopupDataModel() { Description = dr[colname].ToString(), IsChecked = true });
+                            
+                        //sort!
+                        s.FilterData = StaticCollections.SortSFStagePopup(s.FilterData);
+
+                        e.Column = new DataGridTemplateColumn()
+                        {
+                            Header = s,
+                            HeaderStyle = FindResource("ColumnHeaderStyle") as Style,
+                            HeaderTemplate = FindResource("ColumnHeaderFilterTemplate") as DataTemplate,
+                            CellTemplate = FindResource("SalesFunnelStageTemplate") as DataTemplate,
+                            CellStyle = FindResource("CellStyle") as Style
+                        };
+                        e.Column.SortMemberPath = colname;
+                        break;
+                   
+                    case "StatusColour":
+                        e.Cancel = true;
+                        break;
+                    case "ProjectTypeColour":
+                        e.Cancel = true;
+                        break;
+                    default:
+
+                        if (dt.Columns[colname].ExtendedProperties.ContainsKey("FieldType")
+                            && (int)dt.Columns[colname].ExtendedProperties["FieldType"] != (int)ReportFieldType.SystemAndHidden
+                            && (int)dt.Columns[colname].ExtendedProperties["FieldType"] != (int)ReportFieldType.SystemAndRemoved)
+                        {
+
+                            StaticCollections.FormatGridColumn(ref dt, colname, ref b, ref f);
+
+                            if (dt.Columns[colname].ExtendedProperties.ContainsKey("FieldType") && (int)dt.Columns[colname].ExtendedProperties["FieldType"] == (int)ReportFieldType.PlaybookComments)
+                            {//comments
+                                e.Column = new DataGridTemplateColumn()
+                                {
+                                    Header = dt.Columns[colname].Caption,
+                                    Width = 120,
+                                    HeaderStyle = FindResource("ColumnHeaderStyle") as Style,
+                                    CellTemplate = new DataTemplate() { VisualTree = f },
+                                    
+                                };
+                            }
+                            else   //other non-comments colummns                     
+                                StaticCollections.FormatWithNoStatusFilterTemplate(colname, ref dt, this, ref e, ref f, ref dictFilterPopup, Constants.MasterProjectsPopupList);
+                        }
+                        else
+                            e.Cancel = true;
+
+                        break;
+                }
+            }
+            catch
+            {             
+            }
+        }
+                      
+     
+        #region New Business
+
+        //private void NewBusinessGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        //{
+        //    try
+        //    {
+        //        var f = new FrameworkElementFactory(typeof(TextBlock));
+        //        Binding b = new Binding(e.Column.Header.ToString())
+        //        {
+        //            Mode = BindingMode.OneTime
+        //        };
+        //        string colname = e.PropertyName;
+
+        //        DataTable dt = (DataContext as ViewModels.PlaybookViewModel).NewBusiness;
+        //        if (dt.Columns[colname].ExtendedProperties.ContainsKey("FieldType")
+        //                  && (int)dt.Columns[colname].ExtendedProperties["FieldType"] != (int)ReportFieldType.SystemAndHidden
+        //                  && (int)dt.Columns[colname].ExtendedProperties["FieldType"] != (int)ReportFieldType.SystemAndRemoved)
+        //        {                                       
+
+        //            StaticCollections.FormatGridColumn(ref dt, colname, ref b, ref f);
+        //            StaticCollections.FormatWithNoStatusFilterTemplate(colname, ref dt, this, ref e, ref f, ref dictFilterNewBusinessPopup, Constants.NewBusinessProjectsPopupList);
+        //        }
+        //        else
+        //            e.Cancel = true;
+        //    }
+        //    catch
+        //    {
+
+        //    }
+        //}
+
+        #endregion
+
+        
     }
 
+    
 }

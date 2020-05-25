9ffb0f6f9f5ed3e6058db9a1d0144e668f80d78f
Terry <tcdj1963@gmail.com>
2020-05-25 20:18:13 +1200

    Add files via upload

---
+++ Views/EditBUView.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows;
+
+namespace PTR.Views
+{
+    /// <summary>
+    /// Interaction logic for EditBUView.xaml
+    /// </summary>
+    public partial class EditBUView : Window
+    {
+        public EditBUView()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModels.BUViewModel();
+        }
+    }
+}
